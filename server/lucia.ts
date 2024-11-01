793e09e7e0d856ba76f019a3974d20b7fadf78e7
Gia Dat Truong <truonggiadat15@gmail.com>
2024-11-01 22:42:15 +0700

    feat: authentication setup

---
+++ server/lucia.ts
@@ -0,0 +1,25 @@
+import { Lucia } from "lucia";
+
+import { adapter } from "./adapter";
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    attributes: {
+      // set to `true` when using HTTPS
+      secure: process.env.NODE_ENV === "production",
+    },
+  },
+  getUserAttributes(attr) {
+    return {
+      username: attr.username,
+    };
+  },
+});
+
+// IMPORTANT!
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: { username: string };
+  }
+}
